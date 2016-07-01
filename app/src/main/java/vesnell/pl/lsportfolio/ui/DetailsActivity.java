76359010d02599b3e50b5445d19d2a9c6a6f4b24
vesnell <alek666@gmail.com>
2016-07-01 17:26:41 +0200

    add layout to details activity

--- app/src/main/java/vesnell/pl/lsportfolio/ui/DetailsActivity.java
+++ app/src/main/java/vesnell/pl/lsportfolio/ui/DetailsActivity.java
@@ -3,7 +3,12 @@ package vesnell.pl.lsportfolio.ui;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import vesnell.pl.lsportfolio.R;
 import vesnell.pl.lsportfolio.database.model.Project;
@@ -16,14 +21,26 @@ public class DetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
-        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM | ActionBar.DISPLAY_HOME_AS_UP);
-        getSupportActionBar().setCustomView(R.layout.custom_actionbar_text);
-
-        TextView actionBarTitle = (TextView) getSupportActionBar().getCustomView().findViewById(R.id.toolbar_title);
-
         Bundle b = getIntent().getExtras();
         Project project = (Project) b.getSerializable(Project.NAME);
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM | ActionBar.DISPLAY_HOME_AS_UP);
+        getSupportActionBar().setCustomView(R.layout.custom_actionbar_text);
+        TextView actionBarTitle = (TextView) getSupportActionBar().getCustomView().findViewById(R.id.toolbar_title);
         actionBarTitle.setText(project.getName());
+
+        ImageView ivIcon = (ImageView) findViewById(R.id.icon);
+        TextView tvName = (TextView) findViewById(R.id.name);
+        TextView tvDescription = (TextView) findViewById(R.id.description);
+        LinearLayout llGallery = (LinearLayout) findViewById(R.id.gallery);
+        ImageButton ibStoreIos = (ImageButton) findViewById(R.id.store_ios);
+        ImageButton ibStoreAndroid = (ImageButton) findViewById(R.id.store_android);
+        ImageButton ibStoreWindows = (ImageButton) findViewById(R.id.store_windows);
+
+        Picasso.with(this).load(project.getIcon())
+                .resizeDimen(R.dimen.list_item_width, R.dimen.list_item_height).centerCrop().into(ivIcon);
+        tvName.setText(project.getName());
+        tvDescription.setText(project.getDescription());
     }
 
 }
