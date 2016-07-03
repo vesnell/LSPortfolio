6fb9286aa27e89c8a6777467c7407d2d9cd5ecac
vesnell <alek666@gmail.com>
2016-07-03 19:13:00 +0200

    set store buttons

--- app/src/main/java/vesnell/pl/lsportfolio/ui/DetailsActivity.java
+++ app/src/main/java/vesnell/pl/lsportfolio/ui/DetailsActivity.java
@@ -118,15 +118,23 @@ public class DetailsActivity extends AppCompatActivity implements DownloadResult
     }
 
     private void setStores(List<Store> stores) {
-        Store store1 = stores.get(0);
-        Store store2 = stores.get(1);
-        Store store3 = stores.get(2);
-        Picasso.with(this).load(store1.getImage())
-                .resizeDimen(R.dimen.store_item_width, R.dimen.store_item_height).centerCrop().into(ivStore1);
-        Picasso.with(this).load(store2.getImage())
-                .resizeDimen(R.dimen.store_item_width, R.dimen.store_item_height).centerCrop().into(ivStore2);
-        Picasso.with(this).load(store3.getImage())
-                .resizeDimen(R.dimen.store_item_width, R.dimen.store_item_height).centerCrop().into(ivStore3);
+        switch (stores.size()) {
+            case 3:
+                Store store3 = stores.get(2);
+                ivStore3.setVisibility(View.VISIBLE);
+                Picasso.with(this).load(store3.getImage())
+                        .fit().centerInside().into(ivStore3);
+            case 2:
+                Store store2 = stores.get(1);
+                ivStore2.setVisibility(View.VISIBLE);
+                Picasso.with(this).load(store2.getImage())
+                        .fit().centerInside().into(ivStore2);
+            case 1:
+                Store store1 = stores.get(0);
+                ivStore1.setVisibility(View.VISIBLE);
+                Picasso.with(this).load(store1.getImage())
+                        .fit().centerInside().into(ivStore1);
+        }
     }
 
     public void onStoreClick(View v) {
